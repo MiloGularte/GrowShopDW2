b9a96e4fba83835dc71247d5c6b7981cc1f73c78
milo <milagrosgularte54@gmail.com>
2024-06-06 23:11:18 -0300

    estilos

---
+++ js/menuhamb.js
@@ -0,0 +1,7 @@
+
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navbarToggler.addEventListener('click', () => {
+  navbarCollapse.classList.toggle('show');
+});
